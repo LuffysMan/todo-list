822eacad8275a8a972630c26d0530fffbc853336
_luffy <924925465@qq.com>
2021-08-30 11:03:32 +0800

    [Luffy] refactor: should_create_database_when_init

--- src/main/java/todo/commands/CommandExecutor.java
+++ src/main/java/todo/commands/CommandExecutor.java
@@ -6,17 +6,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class CommandExecutor {
-    public CommandResponse execute(String... commandTokens) {
+    public CommandResponse execute(String command) {
+        if (command.equals("init")) {
 
-        try {
-            try (Connection connection = DriverManager.getConnection("jdbc:sqlite:todo.db")) {
-                DatabaseMetaData meta = connection.getMetaData();
-                System.out.println(meta.getDriverName());
+            try {
+                try (Connection connection = DriverManager.getConnection("jdbc:sqlite:todo.db")) {
+                    DatabaseMetaData meta = connection.getMetaData();
+                    System.out.println(meta.getDriverName());
+                }
+            } catch (SQLException e) {
+                return new CommandResponse("failed to connect connect to database", false);
             }
-        } catch (SQLException e) {
-            return new CommandResponse("failed to connect connect to database", false);
+            String path = String.format("Initialized empty todo repository in %s/todo.db", System.getProperty("user.dir"));
+            return new CommandResponse(path, true);
         }
-        String path = String.format("Initialized empty todo repository in %s/todo.db", System.getProperty("user.dir"));
-        return new CommandResponse(path, true);
+        return new CommandResponse("Bad Command: todo <init> | <add> | <list> | <set> | <drop>", false);
     }
 }
