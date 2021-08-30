3aa049a734c75426e3869a1e33f9a1cbc397e3c8
_luffy <924925465@qq.com>
2021-08-30 09:36:19 +0800

    [Luffy] test: should_create_database_when_init

--- src/test/java/todo/story1/TodoInitTest.java
+++ src/test/java/todo/story1/TodoInitTest.java
@@ -1,5 +1,6 @@
 package todo.story1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import todo.commands.CommandExecutor;
 import todo.commands.CommandResponse;
@@ -9,9 +10,13 @@ import static todo.AssertHelper.assertSuccessResponse;
 public class TodoInitTest {
     private CommandExecutor executor;
 
+    @BeforeEach
+    void setupExecutor() {
+        executor = new CommandExecutor();
+    }
     @Test
     void should_create_database_when_init() {
         final CommandResponse response = executor.execute("init");
-        assertSuccessResponse("", response);
+        assertSuccessResponse("Initialized empty todo repository in /home/luffy/Workspace/Project/todo-list/todo.db", response);
     }
 }
