f88cd2a2b460e77429a627cf67c775887a3d7e57
_luffy <924925465@qq.com>
2021-09-20 18:08:44 +0800

    [Luffy] refactor(Story1): extract method

--- src/main/java/todo/commands/CommandExecutor.java
+++ src/main/java/todo/commands/CommandExecutor.java
@@ -1,6 +1,9 @@
 package todo.commands;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class CommandExecutor {
     public CommandResponse execute(String command) {
@@ -13,26 +16,33 @@ public class CommandExecutor {
             }
             try {
                 Class.forName("org.sqlite.JDBC");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
                 try (Connection connection = DriverManager.getConnection("jdbc:sqlite:todo.db")) {
-                    try (Statement statement = connection.createStatement()) {
-                        statement.executeUpdate("CREATE TABLE tasks(" +
-                                "`id` INT NOT NULL, `" +
-                                "name` VARCHAR(32) NOT NULL, " +
-                                "`begin` DATE NOT NULL, " +
-                                "`end` DATE, " +
-                                "`parent` INT NOT NULL DEFAULT 1, " +
-                                "`children` TEXT, " +
-                                "`state` BOOLEAN NOT NULL DEFAULT 0, " +
-                                "`desc` TEXT, PRIMARY KEY(`id`))");
-//                        statement.execute("INSERT INTO tasks name, begin, state, desc VALUES(life, 1992-1-10, 0, life is the task through your life)");
-                    }
+                    createTable(connection);
                 }
-            } catch (SQLException | ClassNotFoundException e) {
+            } catch (SQLException e) {
                 return new CommandResponse(e.getMessage(), false);
             }
             String path = String.format("Initialized empty todo repository in %s/todo.db", System.getProperty("user.dir"));
             return new CommandResponse(path, true);
         }
         return new CommandResponse("Bad Command: todo <init> | <add> | <list> | <set> | <drop>", false);
+    }
+
+    private void createTable(Connection connection) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE tasks(" +
+                    "`id` INT NOT NULL, `" +
+                    "name` VARCHAR(32) NOT NULL, " +
+                    "`begin` DATE NOT NULL, " +
+                    "`end` DATE, " +
+                    "`parent` INT NOT NULL DEFAULT 1, " +
+                    "`children` TEXT, " +
+                    "`state` BOOLEAN NOT NULL DEFAULT 0, " +
+                    "`desc` TEXT, PRIMARY KEY(`id`))");
+        }
     }
 }
